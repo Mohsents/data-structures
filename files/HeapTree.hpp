3e95317507b4b2d9052002afb528ae270dbd2cf2
Mohsents <developer.mohsentimuri@gmail.com>
2024-01-03 17:27:05 +0330

    Add basic functions for Heap Tree

---
+++ files/HeapTree.hpp
@@ -0,0 +1,140 @@
+#include <iostream>
+using namespace std;
+
+class HeapTree {
+
+	private:
+		int *minTree;
+		int *maxTree;
+		int minHSize;
+		int maxHSize;
+		int minCapacity;
+		int maxCapacity;
+
+	public:
+		void createMinHeapTree(int capacity) {
+			this -> minCapacity = capacity;
+			minTree = new int[minCapacity];
+			minHSize = 0;
+		}
+
+		void createMaxHeapTree(int capacity) {
+			this -> maxCapacity = capacity;
+			maxTree = new int[maxCapacity];
+			maxHSize = 0;
+		}
+
+		void swap(int *a, int *b) {
+			int temp = *a;
+			*a = *b;
+			*b = temp;
+		}
+
+		int getParent(int index) {
+			return(index - 1) / 2;
+		}
+
+		void addMin(int item) {
+			if(minHSize == minCapacity) return;
+
+			minHSize++;
+			int i = minHSize - 1;
+			minTree[i] = item;
+
+			while(i != 0 && minTree[i] < minTree[getParent(i)]) {
+				swap(&minTree[i], &minTree[getParent(i)]);
+				i = getParent(i);
+			}
+		}
+
+		void addMax(int item) {
+			if(maxHSize == maxCapacity) return;
+
+			maxHSize++;
+			int i = maxHSize - 1;
+			maxTree[i] = item;
+
+			while(i != 0 && maxTree[i] > maxTree[getParent(i)]) {
+				swap(&maxTree[i], &maxTree[getParent(i)]);
+				i = getParent(i);
+			}
+		}
+
+		void showMinTree() {
+			for(int i = 0; i < minHSize; i++)
+				cout << minTree[i] << " ";
+		}
+
+		void showMaxTree() {
+			for(int i = 0; i < maxHSize; i++)
+				cout << maxTree[i] << " ";
+		}
+
+		void minHeapify(int i) {
+			int left = 2 * i + 1;
+			int rigth = 2 * i + 2;
+			int smallest = i;
+
+			if(left < minHSize && minTree[left] < minTree[i])
+				smallest = left;
+
+			if(rigth < minHSize && minTree[rigth] < minTree[smallest])
+				smallest = rigth;
+
+			if(smallest != i) {
+				swap(&minTree[i], &minTree[smallest]);
+				minHeapify(smallest);
+			}
+	}
+
+		int minRootRemove() {
+			int root;
+			if(minHSize <= 0) return INT_MIN;
+
+			if(minHSize == 1) {
+				minHSize--;
+				return minTree[0];
+			}
+
+			root = minTree[0];
+			minTree[0] = minTree[minHSize - 1];
+			minHSize--;
+			minHeapify(0);
+
+			return root;
+		}
+
+		void maxHeapify(int i) {
+			int left = 2 * i + 1;
+			int rigth = 2 * i + 2;
+			int largest = i;
+
+			if(left < maxHSize && maxTree[left] > maxTree[i])
+				largest = left;
+
+			if(rigth < maxHSize && maxTree[rigth] > maxTree[largest])
+				largest = rigth;
+
+			if(largest != i) {
+				swap(&maxTree[i], &maxTree[largest]);
+				maxHeapify(largest);
+			}
+	}
+
+		int maxRootRemove() {
+			int root;
+			if(maxHSize <= 0) return INT_MAX;
+
+			if(maxHSize == 1) {
+				maxHSize--;
+				return maxTree[0];
+			}
+
+			root = maxTree[0];
+			maxTree[0] = maxTree[maxHSize - 1];
+			maxHSize--;
+			maxHeapify(0);
+
+			return root;
+		}
+};
--- files/main.cpp
+++ files/main.cpp
@@ -9,218 +9,43 @@
 #include "binaryTree.hpp"
 #include "BSTTree.hpp"
 #include "AVLTree.hpp"
+#include "HeapTree.hpp"
 using namespace std;
 
-// void show_array(int array[], int lenght) {
-//     for (int i = 0; i <= lenght; i++) {
-//         cout << "\t" + to_string(array[i]);
-//     }
-// }
-
 int main() {
-    // Define unsorted array
-    // int array[8] = {1,3,6,4};
-    // int sorted_array[15] = {1,2,3,6,8,12,15,22,33,46};
-    // int length = ssize(sorted_array);
-    // int key = -1;
+	HeapTree htree;
+	htree.createMinHeapTree(6);
+	htree.addMin(1);
+	htree.addMin(2);
+	htree.addMin(3);
+	htree.addMin(4);
+	htree.addMin(5);
+	htree.addMin(6);
+	cout << "Min Heap Tree: ";
+	htree.showMinTree();
+	cout << endl;
 
-    // int res = linear_search(array,length, key);
-    //binary_search(sorted_array, 0, length - 1, key);
+	htree.createMaxHeapTree(8);
+	htree.addMax(4);
+	htree.addMax(3);
+	htree.addMax(2);
+	htree.addMax(6);
+	htree.addMax(5);
+	htree.addMax(8);
+	htree.addMax(7);
+	htree.addMax(1);
+	cout << "Max Heap Tree: ";
+	htree.showMaxTree();
 
-    /* if(res == -1) {
-        cout << "item not found!";
-    } else {
-        cout << "item found in index: " + to_string(res);
-    }
-    */
+	cout << endl;
+	cout << "Delete min tree root: ";
+	htree.minRootRemove();
+	htree.showMinTree();
 
-   //del(sorted_array, length, key);
-   //show_array(sorted_array, 8);
+	cout << endl;
+	cout << "Delete max tree root: ";
+	htree.maxRootRemove();
+	htree.showMaxTree();
 
-   // insert_unsorted(array, length, 4, 100);
-   // show_array(array, length);
-
-    //insert_sorted(sorted_array, length, 10, key);
-    //show_array(sorted_array, length);
-
-    // int a[7] = {-1,-1,3,4,5,6,7};
-    // int b[2] = {1,2};
-
-    // merge_sorted(a, b, 5, 2);
-
-    // show_array(a, 7);
-
-    // int arr[] = {5,6,7,8,9,1,2,3,4};
-    // int key = 4;
-    // int res = binary_search_unsorted(arr, 9, key);
-    // cout << "Item found in index:" + to_string(res);
-
-    // struct Queue *queue;
-    // queue = create_queue(10);
-    // add(queue, 45);
-    // add(queue, 232);
-    // add(queue, 67);
-
-    // cout << endl;
-
-    // cout << "size: " << queue -> size << endl;
-    // cout << "capacity: " << queue -> capacity << endl;
-
-    // CircularQueue queue(4);
-    // queue.add(5);
-    // queue.add(7);
-    // queLinkedListue.add(3);
-    // queue.show();
-    // cout << endl;
-    // queue.del();
-    // cout << endl;
-    // queue.show();
-    // cout << endl;
-    // queue.add(6);
-    // queue.add(4);
-    // queue.show();
-    // cout << endl;
-    // queue.add(9);
-    // cout << endl;
-
-    // Stack stack = Stack();
-    // stack.push(3);
-    // stack.push(2);
-    // stack.push(5);
-
-    // int res = stack.pop();
-    // cout << to_string(res);
-
-    // LinkedList<int> list;
-    // list.add_begin(1);
-    // list.add_begin(2);
-    // list.add_begin(3);
-    // list.add_last(100);
-    // list.add_last(200);
-    // list.add_last(300);
-    // list.del_head();
-    // list.del_tail();
-    // list.reverse();
-    // list.show();
-    // cout << "List length = " << list.get_length() << "\n";
-
-    // DoublyLinkedList<int> list;
-    // list.add_begin(1);
-    // list.add_begin(2);
-    // list.add_begin(3);
-    // list.add_last(100);
-    // list.add_last(200);
-    // list.add_last(300);
-    // list.del_head();
-    // list.del_head();
-    // list.del_tail();
-    // list.reverse();
-    // list.delete_at(2);
-    // list.delete_at(5);
-    // list.show();
-   // cout << "List length = " << list.get_length() << "\n";
-
-//    CircularDoublyLinkedList<int> list;
-//     list.add_bein(1);
-//     list.add_begin(2);
-//     list.add_begin(3);
-//     list.add_begin(4);
-//     list.add_begin(5);
-//     list.add_last(100);
-//     list.add_last(200);
-//     list.add_last(300);
-    // list.del_head();
-    // list.del_tail();
-    // list.show();
-    // cout << "\n";
-    // list.delete_at(2);
-    // list.delete_at(5);
-    // list.reverse();
-    // list.show();
-    // cout << "\n";
-    // cout << "List length = " << list.get_length() << "\n";
-
-    //BinaryTree<int> tree;
-    //tree.create_tree(1);
-
-    //cout << "Tree heigth: " << tree.get_height(tree.root) << "\n";
-    //cout << "Tree max value: " << tree.get_max(tree.root) << "\n";
-    //cout << "Tree leaf count: " << tree.get_count_leaf(tree.root) << "\n";
-    //cout << "Tree node count: " << tree.get_count(tree.root) << "\n";
-    //tree.show(tree.root);
-    //cout << "\n";
-    //tree.delete_tree(tree.root);
-    //cout << "In-order teraversal:";
-    //tree.in_order_traversal(tree.root);
-
-    //cout << endl;
-    //cout << "Pre-order teraversal:";
-    //tree.pre_order_teraversal(tree.root);
-
-    //cout << endl;
-    //cout << "Level-order traversal:";
-    //tree.level_order_traversal(tree.root);
-    //cout << endl;
-
-    //int item = 5;
-    //cout << "Level of item " << item << " is: " << tree.find_level(tree.root, item) << endl;
-    //cout << "Ancestors of item " << item << " is: " << tree.find_ancestors(tree.root, item) << endl;
-
-    //cout << "Is complete Tree: " << tree.is_complete_tree(tree.root) << endl;
-
-    //int a = 5;
-    //int b = 3;
-    //cout << "LCA items " << a << " and " << b << ": " << tree.find_LCA(tree.root, a, b) -> data << endl;
-
-    //cout << "In-order Traversal threaded tree: ";
-    //tree.in_order_threaded_travers(tree.root);
-    //cout << endl;
-
-    //cout << "Sum of all root to leaf paths: " << tree.root_to_leaf_sum(tree.root, 0) << endl;
-
-//    BSTTree<int> bsttree;
-//    bsttree.insert(bsttree.root, 10);
-//    bsttree.insert(bsttree.root, 5);
-//    bsttree.insert(bsttree.root, 6);
-//    bsttree.insert(bsttree.root, 8);
-//    bsttree.insert(bsttree.root, 20);
-//    bsttree.insert(bsttree.root, 98);
-//    bsttree.insert(bsttree.root, 12);
-//   bsttree.insert(bsttree.root, 23);
-//    bsttree.insert(bsttree.root, 45);
-//    bsttree.insert(bsttree.root, 50);
-
-//    cout << "In-order Traversal: ";
-//    bsttree.inorder_travers(bsttree.root);
-//    cout << endl;
-
-    //int key = 20;
-//    cout << "Is item " << key << " founded?: " << bsttree.search(bsttree.root, key) << endl;
-
-//    bsttree.remove(bsttree.root, key);
-//    cout << "After remove item " << key << endl;
-//    bsttree.inorder_travers(bsttree.root);
-//    cout << endl;
-
-//    int n = 4;
-//    cout << n << "th largest item in tree is:";
-//    bsttree.nth_largest(bsttree.root, n);
-//    cout << endl;
-
-    AVLTree<int> avl_tree;
-    avl_tree.add(avl_tree.root, 1);
-    avl_tree.add(avl_tree.root, 2);
-    avl_tree.add(avl_tree.root, 3);
-    avl_tree.add(avl_tree.root, 4);
-
-    cout << "Preorder teraversal: ";
-    avl_tree.preorder(avl_tree.root);
-    cout << endl;
-
-    int item = 3;
-    cout << "After delete item: " << item << endl;
-    avl_tree.deleteNode(avl_tree.root, item);
-    avl_tree.preorder(avl_tree.root);
- 
-   return 0;
+	return 0;
 }
